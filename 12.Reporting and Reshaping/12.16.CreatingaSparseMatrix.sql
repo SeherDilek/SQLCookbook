6b030ae379175ca7a909c4f15dff61669bbba65c
Seher Dilek <seher.dilekk@gmail.com>
2024-05-03 03:14:49 +0300

    Creating sparse matrix

---
+++ 12.Reporting and Reshaping/12.16.CreatingaSparseMatrix.sql
@@ -0,0 +1,10 @@
+SELECT
+    CASE WHEN deptno = 10 THEN ename ELSE NULL END AS d10,
+    CASE WHEN deptno = 20 THEN ename ELSE NULL END AS d20,
+    CASE WHEN deptno = 30 THEN ename ELSE NULL END AS d30,
+    CASE WHEN job = 'CLERK' THEN ename ELSE NULL END AS clerks,
+    CASE WHEN job = 'MANAGER' THEN ename ELSE NULL END AS mgrs,
+    CASE WHEN job = 'PRESIDENT' THEN ename ELSE NULL END AS prez,
+    CASE WHEN job = 'ANALYST' THEN ename ELSE NULL END AS anals,
+    CASE WHEN job = 'SALESMAN' THEN ename ELSE NULL END AS sales
+FROM EMP;
