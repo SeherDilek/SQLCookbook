af1689bce8b67053fe2c4dcc10ea8de7b7d377d1
Seher Dilek <seher.dilekk@gmail.com>
2024-05-03 03:57:44 +0300

    Performing aggregations over different groups/partitions simultaneously (COUNT OVER)

---
+++ 12.Reporting and Reshaping/12.18.PerformingAggregationsoverDiffGroupsPartitionsSimultanesously.sql
@@ -0,0 +1,7 @@
+SELECT ename,
+       deptno,
+       COUNT(*) OVER (PARTITION BY deptno) deptno_cnt,
+       job,
+       COUNT(*) OVER (PARTITION BY job) job_cnt,
+       COUNT(*) OVER() total
+FROM EMP;
